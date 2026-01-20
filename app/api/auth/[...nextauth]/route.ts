95315342ffa3050ca88470664ff4a0f72b2ff864
sbansal05 <siddhi.bansal0504@gmail.com>
2026-01-20 21:43:31 +0530

    initial commit

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,24 @@
+import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+const handler = NextAuth({
+    providers: [
+        CredentialsProvider({
+            name: "Email",
+            credentials: {
+                username: { label: "Username", "type": "text", placeholder:"username" },
+                password: { label: "Password", "type": "password" }
+            },
+            async authorize(credentials, req) {
+                return {
+                    username: "nextauth",
+                    id: "1",
+                    email: "nextauth@gmail.com"
+                }
+            }
+        })
+    ]
+
+});
+
+export const GET = handler;
+export const POST = handler;
